b9c3c90d015b81d9ffae0ed634d814541dec27a3
Urs Albisser <u.albisser@gmail.com>
2020-07-04 11:03:15 +0200

    start 03-33_HigherOrLower/

---
+++ 03-33_HigherOrLower/app/src/main/java/com/publicept/a03_33_higherorlower/MainActivity.java
@@ -0,0 +1,14 @@
+package com.publicept.a03_33_higherorlower;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
