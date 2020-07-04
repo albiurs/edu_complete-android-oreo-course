b6b5366657cdbee46651809ff99d36e4596b4daf
Urs Albisser <u.albisser@gmail.com>
2020-07-04 12:42:08 +0200

    basic layout

--- 03-33_HigherOrLower/app/src/main/java/com/publicept/a03_33_higherorlower/MainActivity.java
+++ 03-33_HigherOrLower/app/src/main/java/com/publicept/a03_33_higherorlower/MainActivity.java
@@ -3,8 +3,14 @@ package com.publicept.a03_33_higherorlower;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void guess(View view) {
+        Log.i("Info", "Button pressed!");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
