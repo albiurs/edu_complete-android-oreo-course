d680d640eed4aca0792085435ab91a98b9430a67
Urs Albisser <u.albisser@gmail.com>
2020-07-07 13:03:32 +0200

    implement logic

--- 0339_NumberShapes/app/src/main/java/com/publicept/a03_39_numbershapes/MainActivity.java
+++ 0339_NumberShapes/app/src/main/java/com/publicept/a03_39_numbershapes/MainActivity.java
@@ -5,12 +5,53 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public void testNumber(View view) {
 
-        Log.i("Info", "Button pressed");
+        Log.i("Info", "button pressed");
+
+        EditText editText = (EditText) findViewById(R.id.editTextNumber);
+
+        String message;
+
+        if (editText.getText().toString().isEmpty()) {
+
+            message = "Please enter a number";
+
+        } else {
+
+            Number myNumber = new Number();
+
+            myNumber.number = Integer.parseInt(editText.getText().toString());
+
+            message = editText.getText().toString();
+
+            if (myNumber.isSquare() && myNumber.isTriangular()) {
+
+                message += " is square and triangular!";
+
+            } else if (myNumber.isSquare()) {
+
+                message += " is square but not triangular.";
+
+            } else if (myNumber.isTriangular()) {
+
+                message += " is triangular but not square.";
+
+            } else {
+
+                message += " is neither triangular nor square.";
+
+            }
+
+        }
+
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+
     }
 
     @Override
