9443a1119480699b53e7a815997e45bf13ee684c
Urs Albisser <u.albisser@gmail.com>
2020-07-07 12:46:15 +0200

    start with basic layout

---
+++ 0339_NumberShapes/app/src/main/java/com/publicept/a03_39_numbershapes/MainActivity.java
@@ -0,0 +1,21 @@
+package com.publicept.a03_39_numbershapes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void testNumber(View view) {
+
+        Log.i("Info", "Button pressed");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
